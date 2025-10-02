e60648165085c0b64320db0d68e8e7ed6155b918
Zaob <trung.ngo@datanest.vn>
2025-10-02 10:08:36 +0700

    chore: add example form

---
+++ src/components/example-form.tsx
@@ -0,0 +1,60 @@
+'use client'
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+  name: z.string().min(2, {
+    message: "Name must be at least 2 characters.",
+  }),
+})
+
+export function ExampleForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  })
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Your name" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+}
---
+++ src/store/use-count-store.ts
@@ -0,0 +1,13 @@
+import { create } from 'zustand'
+
+interface CountState {
+  count: number
+  increase: (by: number) => void
+  decrease: (by: number) => void
+}
+
+export const useCountStore = create<CountState>()((set) => ({
+  count: 0,
+  increase: (by) => set((state) => ({ count: state.count + by })),
+  decrease: (by) => set((state) => ({ count: state.count - by })),
+}))
